9dcb3729ca9c344f287f491cd38d47652c70ae81
saurabhbhoi <saurabhbhoi8@gmail.com>
2024-08-29 15:00:19 +0530

    first commit

---
+++ starter-template/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Headers } from "@/sections/Header";
+import { HeroSection } from "@/sections/Hero";
+
+export default function Home() {
+  return (
+    <div className="bg-transparent">
+      <Headers />
+    <HeroSection/>
+     </div>
+  );
+}
---
+++ starter-template/src/sections/Hero.tsx
@@ -0,0 +1,11 @@
+import memojiImage from '@/assets/images/memoji-computer.png'
+import Image from "next/Image"
+export const HeroSection = () => {
+  return <div>
+    <div ></div>
+    <div className="container">
+     <Image src={memojiImage} alt="person"/>
+
+    </div>
+  </div>;
+};
